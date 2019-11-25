ba6f7262d435788c6aeb38123c11598bba4aff4e
mattdotmac <matt.ashdene@icloud.com>
2019-11-25 16:25:28 +0000

    Create movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,1 +1,40 @@
 const mongoose = require('mongoose');
+
+const Movie = mongoose.model('Movie', {
+    movie_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    producer: {
+        type: String,
+        required: true,
+    },
+    available_in_3d: {
+        type: Boolean,
+        required: true
+    },
+    age_rating: {
+        type: String,
+        required: true,
+    },
+    oscar_nominations: {
+        type: Number,
+        required: true,
+    },
+    likes: { type: Number },
+    comments: [{
+        user: String,
+        message: String,
+        dateCreated: Number,
+        rating: Number
+        }]
+    });
---
+++ src/routers/movie.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/movies', async (req, res) => {
+    try {
+        const
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
