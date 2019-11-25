25e7037ccb6f9f0073002bf6ce1bc1473579176a
mattdotmac <matt.ashdene@icloud.com>
2019-11-25 04:44:45 +0000

    Create mongoose.js and movie.js

---
+++ src/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+    
+});
